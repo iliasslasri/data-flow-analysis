98a4bd36bd8537cdeecfd3dc0c6b29596c834316
iliasslasri0 <iliass.lasri@outlook.com>
2024-06-10 09:14:14 +0200

    combined dead code and constant analysis

--- while/src/WhileConstantDeadAnalysis.cc
+++ while/src/WhileConstantDeadAnalysis.cc
@@ -52,12 +52,30 @@ struct WhileConstantValue
   }
 };
 
+typedef std::map<int, WhileConstantValue> WhileConstantDomain;
+
+enum WhileReachability
+{
+  REACHABLE,
+  DEAD
+};
+
+struct WhileConstantDeadValue {
+  WhileConstantDomain WConstant;
+  WhileReachability WReachability = DEAD; // Reach. of current bb
+  WhileReachability WReachabilityTakenBranch = DEAD;
+};
+
 bool operator==(const WhileConstantValue &a, const WhileConstantValue &b)
 {
   return (a.Kind == b.Kind && a.Value == b.Value);
 }
 
-typedef std::map<int, WhileConstantValue> WhileConstantDomain;
+bool operator!=(const WhileConstantValue &a, const WhileConstantValue &b)
+{
+  return !(a== b);
+}
+
 
 std::ostream &operator<<(std::ostream &s, const WhileConstantValue &v)
 {
@@ -73,14 +91,28 @@ std::ostream &operator<<(std::ostream &s, const WhileConstantValue &v)
   abort();
 }
 
-struct WhileConstant : public WhileDataFlowAnalysis<WhileConstantDomain>
+extern std::ostream &operator<<(std::ostream &s, WhileReachability r);
+
+bool operator==(const WhileConstantDeadValue &a, const WhileConstantDeadValue &b)
+{
+  return ( a.WConstant == b.WConstant &&
+   a.WReachability == b.WReachability &&
+   a.WReachabilityTakenBranch == b.WReachabilityTakenBranch);
+}
+
+bool operator!=(const WhileConstantDeadValue &a, const WhileConstantDeadValue &b)
+{
+  return !(a == b);
+}
+
+struct WhileConstant : public WhileDataFlowAnalysis<WhileConstantDeadValue>
 {
   std::ostream &dump_first(std::ostream &s,
-                           const WhileConstantDomain &value) override
+                           const WhileConstantDeadValue &value) override
   {
     s << "    [";
     bool first = true;
-    for(const auto&[idx, c] : value)
+    for(const auto&[idx, c] : value.WConstant)
     {
       if (!first)
         s << ", ";
@@ -88,23 +120,48 @@ struct WhileConstant : public WhileDataFlowAnalysis<WhileConstantDomain>
       s << "R" << idx << "=" << c;
       first = false;
     }
+    s << "]";
+    s << "  Reachability:  [";
+
+    // Information on reachability
+    switch (value.WReachability)
+    {
+      case DEAD:
+       s << FRED << "DEAD";
+       break;
+      case REACHABLE:
+       s << FGREEN << "REACHABLE";
+       break;
+    };
+    s << CRESET;
+    s << ", w/ branch :";
+    switch (value.WReachabilityTakenBranch)
+    {
+      case DEAD:
+       s << FRED << "DEAD";
+       break;
+      case REACHABLE:
+       s << FGREEN << "REACHABLE";
+       break;
+    };
+    s << CRESET;
     return s << "]\n";
   }
 
   std::ostream &dump_pre(std::ostream &s,
-                         const WhileConstantDomain &value) override
+                         const WhileConstantDeadValue &value) override
   {
     return s;
   }
 
   std::ostream &dump_post(std::ostream &s,
-                          const WhileConstantDomain &value) override
+                          const WhileConstantDeadValue &value) override
   {
     return dump_first(s, value);
   }
 
   static void updateRegisterOperand(const WhileInstr &instr, unsigned int idx,
-                             WhileConstantDomain &result,
+                             WhileConstantDeadValue &result,
                              WhileConstantValue value)
   {
     const WhileOperand &op = instr.Ops[idx];
@@ -112,7 +169,7 @@ struct WhileConstant : public WhileDataFlowAnalysis<WhileConstantDomain>
     {
       case WREGISTER:
         assert(op.ValueOrIndex >= 0);
-        result[op.ValueOrIndex] = value;
+        result.WConstant[op.ValueOrIndex] = value;
         return;
 
       case WFRAMEPOINTER:
@@ -127,7 +184,7 @@ struct WhileConstant : public WhileDataFlowAnalysis<WhileConstantDomain>
 
   static WhileConstantValue readDataOperand(const WhileInstr &instr,
                                             unsigned int idx,
-                                            const WhileConstantDomain &input)
+                                            const WhileConstantDeadValue &input)
   {
     const WhileOperand &op = instr.Ops[idx];
     switch (op.Kind)
@@ -135,8 +192,8 @@ struct WhileConstant : public WhileDataFlowAnalysis<WhileConstantDomain>
       case WREGISTER:
       {
         assert(op.ValueOrIndex >= 0);
-        auto value = input.find(op.ValueOrIndex);
-        if (value == input.end())
+        auto value = input.WConstant.find(op.ValueOrIndex);
+        if (value == input.WConstant.end())
           return BOTTOM; // register undefined
         else
           return value->second;
@@ -155,15 +212,44 @@ struct WhileConstant : public WhileDataFlowAnalysis<WhileConstantDomain>
     abort();
   }
 
-  WhileConstantDomain transfer(const WhileInstr &instr, const WhileConstantDomain input) override
+  WhileConstantDeadValue transfer(const WhileInstr &instr, const WhileConstantDeadValue input) override
   {
-    WhileConstantDomain result = input;
+    WhileConstantDeadValue result = input;
     const auto &ops = instr.Ops;
+    bool temp_reach = false;
+    // any code after an unreachable branch or return instruction remains unreachable
+    if(input.WReachability == DEAD && input.WReachabilityTakenBranch == DEAD){
+      result.WReachability = DEAD;
+    } else {
+      temp_reach = true;
+    }
     switch(instr.Opc)
     {
       case WBRANCHZ:
+        if(temp_reach){
+          WhileConstantValue cond = readDataOperand(instr, 0, input);
+          if (cond.Kind == CONSTANT) {
+            if (cond.Value == 1){
+              result.WReachabilityTakenBranch = DEAD;
+              result.WReachability = REACHABLE;
+            } else {
+              result.WReachability = DEAD;
+              result.WReachabilityTakenBranch = REACHABLE;
+            }
+          } else {
+            result.WReachability = REACHABLE;
+            result.WReachabilityTakenBranch = REACHABLE;
+          }
+        }
+        break;
       case WBRANCH:
+        result.WReachability = DEAD;
+        result.WReachabilityTakenBranch = REACHABLE;
+        break;
       case WRETURN:
+        result.WReachability = DEAD;
+        result.WReachabilityTakenBranch = DEAD;
+        break;
       case WSTORE:
         // do not write symbolic registers
         break;
@@ -306,36 +392,70 @@ struct WhileConstant : public WhileDataFlowAnalysis<WhileConstantDomain>
       return BOTTOM;
   }
 
-  WhileConstantDomain join(std::list<WhileConstantDomain> inputs) override
+  WhileConstantDeadValue join(std::list<WhileConstantDeadValue> inputs) override
   {
-    WhileConstantDomain result;
-    for(WhileConstantDomain r : inputs)
+    WhileConstantDeadValue result;
+    if( inputs.empty() ){
+      result.WReachability = DEAD;
+    } else {
+      
+    for(WhileConstantDeadValue r : inputs)
     {
-      for(const auto&[idx, value] : r)
-      {
-        WhileConstantValue &resultvalue = result[idx];
-        resultvalue = join(resultvalue, value);
+      for(const auto&[idx, value] : r.WConstant)
+      { // TODO : Verify next condition !!! 
+        // it appers to be ok, for the example ( the bb {x = 9;} won't be reachable
+        // so the value 9, won't be taken into account )
+        if ( r.WReachability == REACHABLE || r.WReachabilityTakenBranch == REACHABLE){
+          WhileConstantValue &resultvalue = result.WConstant[idx];
+          resultvalue = join(resultvalue, value);
+
+          // Reachability
+          if(r.WReachability == REACHABLE){
+            result.WReachability = REACHABLE;
+          }
+          if(r.WReachabilityTakenBranch == REACHABLE){
+            result.WReachabilityTakenBranch = REACHABLE;
+          }
+        }
       }
+    }
     }
 
     return result;
   }
+
+  WhileConstantDeadValue join(const WhileBlock *bb) override {
+    std::list<WhileConstantDeadValue> bbIn;
+    for(const auto &[pred, kind] : bb->Pred){
+      if(kind == WFALL_THROUGH && BBOut[pred].WReachability == REACHABLE)
+        bbIn.emplace_back(BBOut[pred]);
+      else if(kind == WBRANCH_TAKEN && BBOut[pred].WReachabilityTakenBranch == REACHABLE)
+        bbIn.emplace_back(BBOut[pred]);
+    }
+    // indicate reachability of the current basic block bb
+    WhileConstantDeadValue wcdv = join(bbIn); 
+    // We should just stamp on the entry of bb if we are really in entry
+    if(bb->Index == 0){
+      wcdv.WReachability = REACHABLE;
+    }
+    return wcdv;
+  }
 };
 
 
-struct WhileConstantRegisterAnalysis : public WhileAnalysis
+struct WhileConstantDeadValueAnalysis : public WhileAnalysis
 {
   void analyze(const WhileProgram &p) override
   {
-    WhileConstant WCRA;
-    WCRA.analyze(p);
-    WCRA.dump(std::cout, p);
+    WhileConstant WCDA;
+    WCDA.analyze(p);
+    WCDA.dump(std::cout, p);
   };
 
-  WhileConstantRegisterAnalysis() : WhileAnalysis("WCRA",
-                                                  "Constant Register Analysis")
+  WhileConstantDeadValueAnalysis() : WhileAnalysis("WCDA",
+                                                  "Constant Register & Dead Code Analysis")
   {
   }
 };
 
-WhileConstantRegisterAnalysis WCRA;
+WhileConstantDeadValueAnalysis WCDA;
