f0bdc821426d9d89304bee97215f99ba4d415482
iliasslasri0 <iliass.lasri@outlook.com>
2024-06-15 13:35:20 +0200

    finished test spy

---
+++ tp-tdd/src/LightControlSpy.c
@@ -0,0 +1,27 @@
+#include "LightControlSpy.h"
+
+static int lastLightId = LIGHT_ID_UNKNOWN;
+static int lastState = LIGHT_STATE_UNKNOWN;
+
+void LightControl_init(void){
+    lastLightId = LIGHT_ID_UNKNOWN;
+    lastState = LIGHT_STATE_UNKNOWN;
+}
+
+void LightControl_on(int id){
+    lastLightId = id;
+    lastState = LIGHT_ON;
+}
+
+void LightControl_destroy(void){
+    lastLightId = LIGHT_ID_UNKNOWN;
+    lastState = LIGHT_STATE_UNKNOWN;
+}
+
+int LightControlSpy_getLastLightId(void) {
+    return lastLightId;
+}
+
+int LightControlSpy_getLastState(void) {
+    return lastState;
+}
